dc50b418ac5ecc8acb8b0836a7dc983d513e846f
jacobpelletier <jacobjpelletier@gmail.com>
2018-09-17 16:19:00 -0400

    Changed name of "hw091018converter.py" to "hw091018Dec_toBin_converter.py" to reflect more specifically what this module can convert

--- submitted/hw091018Dec_to_Bin_converter.py
+++ submitted/hw091018Dec_to_Bin_converter.py
@@ -5,7 +5,7 @@ Created on Fri Sep  7 17:02:49 2018
 
 @author: jacobpelletier
 
-the equation used to convert a number from decimal form to binary form is called 
+the equation used to convert a number from decimal form to binary form is called
 the 'base-q expansion form'
 
 we can use the 'repeated division algorithm' to convert decimal form to binary form
@@ -14,7 +14,7 @@ Sources:
     1) 'https://www.cs.uky.edu/~jzhang/CS321/lecture1.pdf'
     2) 'https://blog.angularindepth.com/the-simple-math-behind-decimal-binary-
      conversion-algorithms-d30c967c9724'
-    
+
 """
 
 def dec_to_bin_converter(user_input):
@@ -35,7 +35,3 @@ def dec_to_bin_converter(user_input):
 # debug tests
 #user_input = 15
 #print(ccc(user_input))
-
-     
-    
-
